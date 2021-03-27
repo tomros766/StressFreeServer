7ee4b11e7382b8416f56d0d9c499b7042bedc1ed
Rados13 <radoslawszuma@gmail.com>
2021-03-27 22:27:09 +0100

    YT api added

--- src/server.js
+++ src/server.js
@@ -12,6 +12,17 @@ app.get('/', function (req, res) {
     res.send('Hello GET');
  })
 
+const playlistId = "PLOWSbqDrrI5QU6-kEuXq2eFqWZ3po6yHh"
+const url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&maxResults=1000&playlistId="+playlistId+"&key="
+const yt_video = "https://www.youtube.com/watch?v="
+
+app.get('/clips', (req,res) => {
+   fetch(url+process.env.YT_API_KEY)
+   .then(response => response.json())
+   .then(data => res.send(data.items.map(elem =>yt_video+elem.snippet.resourceId.videoId)))
+   .catch(err => console.log(err));
+})
+
 app.get('/categories', function (req, res) {
    fs.readFile( __dirname + "/resources/" + "categories.json", 'utf8', function (err, data) {
       console.log( data );
@@ -22,7 +33,7 @@ app.get('/categories', function (req, res) {
 app.get('/meme', function (req, res) {
     const obj = fetch('https://meme-api.herokuapp.com/gimme')
     .then(response => response.json())
-    .then(data => res.send(data.url))
+    .then(data => res.send(data))
     .catch(err => console.log(err));
 })
 
