1e4418dffa5fae1e525b2209d1b2abb945dfc03c
tomros766 <trosiek4@gmail.com>
2021-03-27 18:56:36 +0100

    cleanup

--- src/server.js
+++ src/server.js
@@ -15,8 +15,4 @@ app.get('/getCategories', function (req, res) {
    });
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(8081)
