4e1e4fd895ef91b7cf786abd26d4c9afb9e31a2e
Rados13 <radoslawszuma@gmail.com>
2021-03-27 22:40:23 +0100

    Small fixes in endpoint name and return value of clip

--- src/server.js
+++ src/server.js
@@ -18,10 +18,15 @@ const playlistId = "PLOWSbqDrrI5QU6-kEuXq2eFqWZ3po6yHh"
 const url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&maxResults=1000&playlistId="+playlistId+"&key="
 const yt_video = "https://www.youtube.com/watch?v="
 
-app.get('/clips', (req,res) => {
+app.get('/clip', (req,res) => {
    fetch(url+process.env.YT_API_KEY)
    .then(response => response.json())
-   .then(data => res.send(data.items.map(elem =>yt_video+elem.snippet.resourceId.videoId)))
+   .then(data => 
+      {
+         const urls = data.items.map(elem =>yt_video+elem.snippet.resourceId.videoId)
+         const idx = Math.floor(Math.random()*Math.floor(urls.length))
+         res.send({"url": urls[idx]})
+      })
    .catch(err => console.log(err));
 })
 
